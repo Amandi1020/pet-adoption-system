bbe482c5957cfdaaa0c33d035cadbea12bea9ae9
Amandi1020 <amandirajapaksha2003@gmail.com>
2026-05-11 12:14:39 +0530

    Add Navbar component with purple CSS styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/pets">Browse Pets</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+    <nav className="navbar">
+      <div className="navbar-brand">
+        🐾 PawFind
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/pets" className="nav-link">Browse Pets</Link>
+        <Link to="/login" className="nav-link">Login</Link>
+        <Link to="/register" className="nav-link register-btn">Register</Link>
+      </div>
     </nav>
   )
 }
