97b0c719029749dd56da5cc21b221c33a5a721f4
Amandi1020 <amandirajapaksha2003@gmail.com>
2026-05-12 22:02:25 +0530

    Update App.jsx to include Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import PetList from './pages/PetList'
 import Login from './pages/Login'
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
