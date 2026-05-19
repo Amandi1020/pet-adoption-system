9fdb4c85770a4fb0e621f0ac3ca94be55c851c17
Amandi1020 <amandirajapaksha2003@gmail.com>
2026-05-19 11:06:52 +0530

    Add adoption application route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AdoptForm from './pages/AdoptForm'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -15,6 +16,7 @@ function App() {
         <Route path="/pets" element={<PetList />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/adopt" element={<AdoptForm />} /> 
       </Routes>
       <Footer />
     </BrowserRouter>
